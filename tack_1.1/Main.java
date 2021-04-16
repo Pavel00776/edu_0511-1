30fddeecc9ce544c628d7a160d70c6fd43125a99
Павел <82586985+Pavel00776@users.noreply.github.com>
2021-04-16 12:23:12 +0300

    Create Main.java

---
+++ tack_1.1/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+       String name = "Иван";
+       String city = "Москва";
+       int age = 28;
+        System.out.println("Hello "+name+" "+city+" - "+age+" лет");
+    }
+}
